ef26e3ae10074428385110c26d5a6174891dc80e
Ren <76534076+rendiscorddev@users.noreply.github.com>
2021-02-20 14:09:16 -0500

    Add files via upload

---
+++ rendlx.js
@@ -0,0 +1,23 @@
+
+
+module.exports = {
+    Version: require("/package.json").version,
+    ChangeMyMind: require("src/indexindex.js").ChangeMyMind,
+    Chat: require("src/indexindex.js").Chat,
+    Clyde: require("src/indexindex.js").Clyde,
+    DiscordJSDocs: require("src/indexindex.js").DiscordJSDocs,
+    Gay: require("src/indexindex.js").Gay,
+    GetAdvice: require("src/indexindex.js").GetAdvice,
+    GetAnimalImage: require("src/indexindex.js").GetAnimalImage,
+    GetAnimeImage: require("src/indexindex.js").GetAnimeImage,
+    GetFact: require("src/indexindex.js").GetFact,
+    GetJoke: require("src/indexindex.js").GetJoke,
+    GetMeme: require("src/indexindex.js").GetMeme,
+    GetWhy: require("src/indexindex.js").GetWhy,
+    Triggered: require("src/indexindex.js").Triggered,
+    TrumpTweet: require("src/indexindex.js").TrumpTweet,
+    Tweet: require("src/indexindex.js").Tweet,
+    Wasted: require("src/indexindex.js").Wasted,
+    WhoWouldWin: require("src/indexindex.js").WhoWouldWin,
+    YoutubeComment: require("src/indexindex.js").YoutubeComment
+};
